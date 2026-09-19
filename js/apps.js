04c648b49c5f4bffa18f3be83e0849431508d5c7
GALANACCI THE CREATOR <francis@galanacci.com>
2026-09-19 20:43:59 +0100

    chore: update PoG entry URL

--- js/apps.js
+++ js/apps.js
@@ -3,7 +3,7 @@ export const APPS = Object.freeze([
     id: "pog-exe",
     label: "PoG.EXE",
     icon: new URL("../assets/icons/PoG.EXE.svg", import.meta.url).href,
-    url: "https://pioneersofgreatness.com/?preview=loading",
+    url: "https://pioneersofgreatness.com/?entry=galanacci",
     enabled: true
   },
   {
